f9c6b4c4ccf7c9222555aafcb311c64fc18643bf
pierre paci <pierre.paci@gmail.com>
2018-08-15 11:08:28 +0200

    use different color or transparent background for images

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import argparse
+from argparse import ArgumentTypeError
 from pathlib import Path
 
 from wrk2img import Parser, ImageGenerator
 
 
 def parse_args():
+    def color(value):
+        if len(value) != 6:
+            raise ArgumentTypeError("Invalid color. Should be hexadecimal like FFFFFF")
+        else:
+            return value
+
     parser = argparse.ArgumentParser(description="Transforming wrk results to image", usage="wrk --latency http://localhost:8080 | wrk2img output.png")
     parser.add_argument("output", type=Path, help="output file")
-    # parser.add_argument("-b", "--background", type=str, help="background color")
-    # parser.add_argument("-t", "--transparent", type=str, help="use a transparent background")
+    parser.add_argument("-b", "--background", type=color, default="FFFFFF", metavar="FFFFFF", help="background color")
+    parser.add_argument("-t", "--transparent", action="store_true", help="use a transparent background")
     return parser.parse_args()
 
 
 def cli():
-    parser = Parser()
-    image_generator = ImageGenerator()
     args = parse_args()
+
+    parser = Parser()
+    image_generator = ImageGenerator(transparent=args.transparent, background=args.background)
 
     data, website = parser.parse_stdin()
     image_generator.generate_and_save_image(data, website, args.output)
+
 
 # TODO: Create another cli to generate multiple wrk2 call to one website and generate plot
 
--- wrk2img/image_generator.py
+++ wrk2img/image_generator.py
@@ -10,6 +10,10 @@ from matplotlib.figure import Figure
 
 
 class ImageGenerator:
+    def __init__(self, transparent: bool, background: str):
+        self.transparent = transparent
+        self.background = "#" + background
+
     # TODO: implement background color and transparent option
     def generate_and_save_image(self, data: Dict[float, Dict[float, float]], website: str, output: Path):
         figure = self.generate_image(data, website)
@@ -20,14 +24,15 @@ class ImageGenerator:
         ax = fig.add_subplot(1, 1, 1)  # type: Axes
         for label, values in data.items():
             x, y = zip(*sorted(values.items()))
-            y_ms = [v*1000 for v in y]
+            y_ms = [v * 1000 for v in y]
             ax.plot(x, y_ms, label=str(label) + " req/s")
-        ax.set_ylim(bottom=max(ax.get_ylim()[0],0))
+        ax.set_ylim(bottom=max(ax.get_ylim()[0], 0))
         ax.legend()
         ax.set(title="Latency graph for %s" % website,
                xlabel="percentile",
-               ylabel="latency [ms]")
+               ylabel="latency [ms]",
+               facecolor=self.background)
         return fig
 
     def save_image(self, figure: Figure, output: Path):
-        figure.savefig(str(output))
+        figure.savefig(str(output), transparent=self.transparent, facecolor=self.background)

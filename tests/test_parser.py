e2b1ff4eaff8d2d13d5478401d00bd55f5f2edc4
pierre paci <pierre.paci@gmail.com>
2018-12-18 08:29:36 +0100

    refactored test structure for scalability

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
 import unittest
+from glob import glob
 from pathlib import Path
 
 from wrk2img import Parser
@@ -14,19 +15,25 @@ class TestParser(unittest.TestCase):
             'wrk': ({748868.53: {50: 250e-6, 75: 491e-6, 90: 700e-6, 99: 5.8e-3}}, 'localhost:8080'),
             'localhost': ({643.28: {50: 15.25e-3, 75: 15.46e-3, 90: 15.79e-3, 99: 22.67e-3, }}, '127.0.0.1:5000')
         }
-        for example in ['localhost', 'wrk', 'wrk2']:
-            path = Path('example').joinpath(example)
+        for path in glob('example/mono/wrk/*'):
+            path = Path(path)
             with path.open() as file:
                 wrk_output = file.read()
             self.assertEqual(str, type(wrk_output))
             parsed = self.parser.parse_wrk_output(wrk_output)
-            if path.stem != 'wrk2':
-                self.assertEqual(expected_results[str(path.stem)], parsed)
-            else:
-                self.assertGreater(len(parsed[0][2000.28]), 20)
+            self.assertEqual(expected_results[str(path.stem)], parsed)
+
+    def test_parse_wrk2_output(self):
+        for path in glob('example/mono/wrk2/*'):
+            path = Path(path)
+            with path.open() as file:
+                wrk_output = file.read()
+            self.assertEqual(str, type(wrk_output))
+            parsed = self.parser.parse_wrk_output(wrk_output)
+            self.assertGreater(len(parsed[0][2000.28]), 20)
 
     def test_parse_multiple_wrk_output(self):
-        path = Path('example').joinpath('wrk_cat')
+        path = Path('example/multi').joinpath('wrk_cat')
         with path.open() as file:
             wrk_output = file.read()
         parsed = self.parser.parse_wrk_output(wrk_output)

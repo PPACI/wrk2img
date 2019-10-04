783928d26a72b96f58b0da7a70c24073299cb0d0
Francois Farquet <farquet.francois@gmail.com>
2019-10-04 16:41:09 +0200

    Add support for --max-latency to cut the tail latency at a given value

--- wrk2img/image_generator.py
+++ wrk2img/image_generator.py
@@ -15,15 +15,16 @@ class ImageGenerator:
         self.background = '#' + background
         self.scale = 'log' if log_scale else 'linear'
 
-    def generate_and_save_image(self, data: Dict[str, Dict[float, float]], website: str, output: Path):
-        figure = self.generate_image(data, website)
+    def generate_and_save_image(self, data: Dict[str, Dict[float, float]], website: str, output: Path, latency_until: float = 100):
+        figure = self.generate_image(data, website, latency_until=latency_until)
         self.save_image(figure, output)
 
-    def generate_image(self, data: Dict[str, Dict[float, float]], website: str) -> Figure:
+    def generate_image(self, data: Dict[str, Dict[float, float]], website: str, latency_until: float = 100) -> Figure:
         fig = plt.figure(figsize=(7, 5))
         ax = fig.add_subplot(1, 1, 1)  # type: Axes
         for label, values in data.items():
-            x, y = zip(*sorted(values.items()))
+            filtered_values = {p: v for p, v in values.items() if p <= latency_until}
+            x, y = zip(*sorted(filtered_values.items()))
             y_ms = [v * 1000 for v in y]
             ax.plot(x, y_ms, label=label)
         if self.scale == 'linear':
